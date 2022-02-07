f5b32d384a85d7bf26be427959b43d41ab2bc9b6
Ivan Silva <ivankuartz@hotmail.com>
2022-02-07 17:42:30 -0300

    Book list being added and displayed correctly

--- books.js
+++ books.js
@@ -1,5 +1,8 @@
+
+const book1 = { title: 'first book', author: 'first author', id: 1 }
+
 // Create a collection that keeps a list of books (hint: you can use an array of objects for that).
-const listBooks = [{title: 'first book', author: 'first author', id:1},{title: 'second book', author: 'second author', id:2} ];
+const listBooks = [];
 
 function BookConstructor(title, author, id) {
 this.title = title;
@@ -17,10 +20,37 @@ const book1Title = document.querySelector('#book1-title');
 const book1Author = document.querySelector('#book1-author');
 const inputAdd = document.querySelector('#input-add');
 
-inputAdd.addEventListener('click', () => {
-  book1Title.textContent = inputTitle.value;
-  book1Author.textContent = inputAuthor.value;
+const booksSection = document.querySelector('#booklist');
 
+/* eslint-disable no-tabs */
+function generateHTML(details) {
+  const template = `
+        <div id="book${details.id}">
+            <p id="book${details.id}-title">${details.title}</p>
+            <p id="book${details.id}-author">${details.author}</p>
+            <button id="book${details.id}-remove">Remove</button>
+        </div>
+      `;
+  return template;
+}
+
+// Function to create object
+inputAdd.addEventListener('click', () => {
+//   book1Title.textContent = inputTitle.value;
+//     book1Author.textContent = inputAuthor.value;
+    const addedBook = Object.create(book1);
+    addedBook.title = inputTitle.value;
+    addedBook.author = inputAuthor.value;
+    addedBook.id = listBooks.length + 1;
+    listBooks.push(addedBook);
+    console.log(listBooks);
+    const arrayBook = [addedBook];
+      // eslint-disable-next-line array-callback-return
+    arrayBook.map((book) => {
+        const HTMLElement = document.createElement('div');
+        HTMLElement.innerHTML = generateHTML(book);
+        booksSection.appendChild(HTMLElement);
+    });
 })
 
 
