0f5cc07763021ce3d67fb3211b51c3a6b1adda2d
yersel <yersel500@gmail.com>
2022-02-08 18:51:38 -0500

    Fix linters comments

--- books.js
+++ books.js
@@ -1,89 +1,98 @@
 // Collecting information from inputs
-const inputTitle = document.querySelector('#input-title')
-const inputAuthor = document.querySelector('#input-author')
-const inputAdd = document.querySelector('#input-add')
-const booksSection = document.querySelector('#booklist')
-
-const book1 = { title: 'first book', author: 'first author', id: 1 }
+const inputTitle = document.querySelector('#input-title');
+const inputAuthor = document.querySelector('#input-author');
+const inputAdd = document.querySelector('#input-add');
+const booksSection = document.querySelector('#booklist');
 
 // Create a collection that keeps a list of books (hint: you can use an array of objects for that).
-let listBooks = []
-class ReplaceBooks {
-    replacebooks(title, author, id) {
-            this.title = title,
-            this.author = author,
-            this.id = id
-    }
-    addBook() {
-      booksSection.innerHTML = '';
-      listBooks.map((book) => {
-      const HTMLElement = document.createElement('div');
-      HTMLElement.innerHTML = generateHTML(book);
-      booksSection.appendChild(HTMLElement);
-      });
-      // Updating local storage for books
-      localStorage.setItem('listBooks', JSON.stringify(listBooks));
-    }
-    deleteBook(deleteBook) {
-      listBooks = listBooks.filter(book => book.id !== deleteBook)
-      booksSection.innerHTML = ''
-      listBooks.forEach((book) => {
-      const HTMLElement = document.createElement('div')
-      HTMLElement.innerHTML = generateHTML(book)
-      booksSection.appendChild(HTMLElement)
-      })
-      //Updating local storage for books
-      localStorage.setItem('listBooks', JSON.stringify(listBooks));
-    }
-}
-    // don't forget to check if empty books are not being added to list
+let listBooks = [];
 
-const book2 = new ReplaceBooks();
-
-function generateHTML (details) {
+function generateHTML(details) {
   const template = `
   <div id="book${details.id}">
       <p id="book${details.id}-details">${details.title} by ${details.author}</p>
       <button id="book${details.id}-remove" class='remove-book'>Remove</button>
   </div>
-`
-return template
+`;
+  return template;
 }
+
+class ReplaceBooks {
+  replacebooks(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+
+  generateContent(details) {
+    const template = `
+    <div id="book${details.id}">
+        <p id="book${details.id}-details">${details.title} by ${details.author}</p>
+        <button id="book${details.id}-remove" data-id='${this.id}' class='remove-book'>Remove</button>
+    </div>
+    `;
+    return template;
+  }
+
+  addBook() {
+    booksSection.innerHTML = '';
+    listBooks.forEach((book) => {
+      const HTMLElement = document.createElement('div');
+      HTMLElement.innerHTML = this.generateContent(book);
+      booksSection.appendChild(HTMLElement);
+    });
+    // Updating local storage for books
+    localStorage.setItem('listBooks', JSON.stringify(listBooks));
+  }
+
+  deleteBook(trydeleteBook) {
+    listBooks = listBooks.filter((book) => book.id !== trydeleteBook);
+    booksSection.innerHTML = '';
+    listBooks.forEach((book) => {
+      const HTMLElement = document.createElement('div');
+      HTMLElement.innerHTML = this.generateContent(book);
+      booksSection.appendChild(HTMLElement);
+    });
+    // Updating local storage for books
+    localStorage.setItem('listBooks', JSON.stringify(listBooks));
+  }
+}
+
+const book2 = new ReplaceBooks();
 
 // Data is preserved in localStorage
 window.onload = function storeData() {
-      // Mantain data on the form
-    let dataStored = {}
-    inputTitle.addEventListener('change', (event) => {
-    dataStored = { ...dataStored, inputTitle: event.target.value }
-    localStorage.setItem('dataStored', JSON.stringify(dataStored))
-  })
+  // Mantain data on the form
+  let dataStored = {};
+  inputTitle.addEventListener('change', (event) => {
+    dataStored = { ...dataStored, inputTitle: event.target.value };
+    localStorage.setItem('dataStored', JSON.stringify(dataStored));
+  });
 
   inputAuthor.addEventListener('change', (event) => {
-    dataStored = { ...dataStored, inputAuthor: event.target.value }
-    localStorage.setItem('dataStored', JSON.stringify(dataStored))
-  })
+    dataStored = { ...dataStored, inputAuthor: event.target.value };
+    localStorage.setItem('dataStored', JSON.stringify(dataStored));
+  });
 
   if (localStorage.getItem('dataStored')) {
-    dataStored = JSON.parse(localStorage.getItem('dataStored'))
+    dataStored = JSON.parse(localStorage.getItem('dataStored'));
 
-    inputTitle.value = dataStored.inputTitle
-    inputAuthor.value = dataStored.inputAuthor
+    inputTitle.value = dataStored.inputTitle;
+    inputAuthor.value = dataStored.inputAuthor;
   }
 
   // Mantain data on the book container
   if (localStorage.getItem('listBooks')) {
-    listBooks = JSON.parse(localStorage.getItem('listBooks'))
+    listBooks = JSON.parse(localStorage.getItem('listBooks'));
     listBooks.forEach((book) => {
-      const HTMLElement = document.createElement('div')
-      HTMLElement.innerHTML = generateHTML(book)
-      booksSection.appendChild(HTMLElement)
-    })
+      const HTMLElement = document.createElement('div');
+      HTMLElement.innerHTML = generateHTML(book);
+      booksSection.appendChild(HTMLElement);
+    });
   }
-}
+};
 
-
-const bookList = document.querySelector('#booklist')
+const bookList = document.querySelector('#booklist');
 
 // Function to create object
 inputAdd.addEventListener('click', () => {
@@ -93,11 +102,11 @@ inputAdd.addEventListener('click', () => {
   book3.id = listBooks.length + 1;
   listBooks.push(book3);
   book3.addBook();
-})
+});
 
 // Removing books
 bookList.addEventListener('click', (e) => {
-    let deleteBook = e.target.getAttribute('id')
-    deleteBook = parseInt(deleteBook.replace(/\D/g, ''), 10)
-    book2.deleteBook(deleteBook)
-})
+  let deleteBook = e.target.getAttribute('id');
+  deleteBook = parseInt(deleteBook.replace(/\D/g, ''), 10);
+  book2.deleteBook(deleteBook);
+});
