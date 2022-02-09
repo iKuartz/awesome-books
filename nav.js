0d9b171f09a59dca02cce08ee2b09f9f7b83520a
Ivan Silva <ivankuartz@hotmail.com>
2022-02-09 14:38:07 -0300

    Started implementing the javascript changes

---
+++ nav.js
@@ -0,0 +1,16 @@
+const bookshelf = document.getElementById('bookshelf')
+const addBook = document.getElementById('add-book')
+const contactInfo = document.getElementById('contact-info')
+
+const menuList = document.getElementsByClassName('header-links')
+
+
+function handleNavigation(){
+    if 
+}
+
+menuList.array.forEach(element => {
+element.addEventListener('click', handleNavigation)    
+});
+
+
