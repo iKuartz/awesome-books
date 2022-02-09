d4402ee4f91dbb266b8e744d4fdd325e82f40ba8
mengiefen <mengiefen@gmail.com>
2022-02-09 22:29:36 +0300

    modify the menu navigation

--- nav.js
+++ nav.js
@@ -2,15 +2,29 @@ const bookshelf = document.getElementById('bookshelf')
 const addBook = document.getElementById('add-book')
 const contactInfo = document.getElementById('contact-info')
 
-const menuList = document.getElementsByClassName('header-links')
+const menuList = document.querySelectorAll('.header-links')
 
 
-function handleNavigation(){
-    if 
+function handleNavigation(index) {
+  if (menuList[index].innerHTML === 'List') {
+    bookshelf.style.display = 'flex';
+    addBook.style.display = 'none';
+    contactInfo.style.display = 'none';
+  }
+  if (menuList[index].innerHTML === 'Add new') {
+    bookshelf.style.display = 'none';
+    addBook.style.display = 'flex';
+    contactInfo.style.display = 'none';
+  }
+  if (menuList[index].innerHTML === 'Contact') {
+    bookshelf.style.display = 'none';
+    addBook.style.display = 'none';
+    contactInfo.style.display = 'flex';
+  }
 }
 
-menuList.array.forEach(element => {
-element.addEventListener('click', handleNavigation)    
+menuList.forEach((element, index) => {
+  element.addEventListener('click', () => {
+    handleNavigation(index);
+  })
 });
-
-
